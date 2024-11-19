dafd6d3450fb79c52da86cd1f2125b3056e7d013
arpita <arpita@stockholmitacaedmy.org>
2024-11-19 14:33:23 +0100

    added git workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   watchForFileChanges:false,
   e2e: {
-    chromeWebSecurity:false,
+    projectId: "ueep6p",
+    //chromeWebSecurity:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
